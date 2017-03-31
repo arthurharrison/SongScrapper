92bf460d458a7f65c114c36368de4d19cf06f8d6
Arthur Tavares <Arthur Tavares>
2017-03-31 13:02:32 -0300

    New Cases in Tries Tree

--- TreeTrie.py
+++ TreeTrie.py
@@ -1,6 +1,6 @@
-value = 0
+value = 0 #unnecessary
 def newTrie(*words):
-    print(words)
+    #print(words) #debug
     tree = dict() #the tree as a dictionary
     for word in words:
         currentDict = tree
@@ -14,4 +14,49 @@ def newTrie(*words):
             
     return tree
 
-print(newTrie('bahea','salvado','salvador','sal','sal','bahea','bahia','sergipe'))
+#you need to pass the Trie Tree and the word you are looking for
+def inTrie(trie, word):
+    currentDict = trie
+    for letter in word:
+        #it runs every letter
+        if(letter in currentDict):
+            #it will run until the last letter found
+            currentDict = currentDict[letter]
+        else:
+            return False
+    else:
+        #if it runs and FIND ALL LETTERS IN THE WORD
+        #it will look if it has a value on it
+        if ('value' in currentDict):
+            return True
+        else:
+            return False
+
+#remover
+#In Progress
+def remTrie(trie,word):
+    currentDict = trie
+    for letter in word:
+        if(letter in currentDict): #it will run until the last letter found
+            currentDict = currentDict[letter]
+    else:
+        if('value' in currentDict): # the last letter of a phrase
+            if(len(currentDict) == 1):
+                del[currentDict['value']]
+            elif(len(currentDict) > 1):
+                del[currentDict['value']]
+    #print(currentDict)
+    #print(trie) Debug
+    return trie
+            
+            
+                
+                
+
+    
+    
+arvore = newTrie('bahea','salvado','salvador','sal','sal','bahea','bahia','sergipe')
+#print(arvore)
+#print(inTrie(arvore,'ser'))
+#teste = remTrie(arvore,'salvador')
+#print(teste)
