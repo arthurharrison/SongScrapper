1057ac50c125dfb7ef6d9fd17e030e4b1099ed87
Arthur Tavares <tavares593@gmail.com>
2017-09-15 23:48:09 -0300

    Finishing OOP update and some small fixes with variables use

--- TrieOOP.py
+++ TrieOOP.py
@@ -1,55 +1,50 @@
 class TreeTrie:
     #---------------
-    #some mistakes of security were made
-    #global teste
-    #global tete
-    #tete = ''
+    #Do I Have something to say?
+    #CurrentDict = the variable that is going to get in the Dictonary and explore every branch of it
+    #progLetter = is the Progression of the word, it starts with the first letter and in the end will be the whole word
+    #
     #--------------
 
-
-    teste = [] #inicializando variavel necessária
-    #Function newTrie
     def __init__(self, *words):
         tree = dict() #the tree as a dictionary
 
         for word in words:
             currentDict = tree
-            c = ''
+            progLetter = ''
             for letter in word:
-                c = c + letter
-                currentDict = currentDict.setdefault(c,{})
-                #currentDict['value'] = value
+                progLetter = progLetter + letter
+                currentDict = currentDict.setdefault(progLetter,{})
             if('value' in currentDict):
                 currentDict['value'] = currentDict['value'] + 1
             else:
                 currentDict['value'] = 1
-                
-        #return tree
+
         self.tree = tree #ATTENTION
 
     #Function that adds words in a already existed Trie's Tree
     #It works very similar to the creation of a new Tree's
     def addTrie(self,*words):
-        currentDic = self.tree                       #trie #It  receives the Tree's to a variable that will change TESTE
 
-
-        for word in words:
-            currentDict = currentDic #CurrentDic will bet the variable that is going to get in the Dictonary
-            c = ''
-            for letter in word:
-                c = c + letter
-                if(c in currentDict):
-                    currentDict = currentDict[c] #This will enable that if the letter already exists it will just get in it, NOT CREATE A NEW
+        for word in words: #looping all words given
+            currentDict = self.tree 
+            progLetter = ''
+            for letter in word: #creating every letter of the given word
+                progLetter = progLetter + letter
+                if(progLetter in currentDict): #This will enable that if the letter already exists it will just get in it, NOT CREATE A NEW
+                    currentDict = currentDict[progLetter]
                 else:
-                    currentDict = currentDict.setdefault(c,{}) #And if not, it will Create one
-            if('value' in currentDict): 
+                    currentDict = currentDict.setdefault(progLetter,{}) #And if not, it will Create one
+
+            if('value' in currentDict): #it will enter in the final letter of the word
                 currentDict['value'] = currentDict['value'] + 1  #If for some reason the word already exists, it will only Add +1 to the Counter
             else:
                 currentDict['value'] = 1 #If not, it will create the Counter
                 
-        return currentDic #Returns the Trie's Tree
+        return self.tree
 
-
+#-------------
+#TODO: Delete ?
     def percorra(self, trie):
         aux = list(self.tree)
         tete = ''
@@ -69,7 +64,9 @@ class TreeTrie:
             #print(inTrie(trie,tete))#debugger
             if(type(self.tree[aux[i]]) == int): #or type(aux[i]) == int):
                 continue
-            self.percorra(self.tree[aux[i]])
+            return self.percorra(self.tree[aux[i]])
+        
+
 
     #creates a list with all the itens found in the tree with value
     def percorraTor(self, lista):
@@ -81,17 +78,18 @@ class TreeTrie:
                 tec.append(lista[i])
             
         return tec
-
+#------------------
+    
     #It gets a value of a word
     def getVal(self, word):
-        if(not(inTrie(self.tree,word))):
+        if(not(self.inTrie(word))):
             return 0
         currentDict = self.tree
-        c = ''
+        progLetter = ''
         for letter in word:
-            c = c + letter
-            if(c in currentDict):
-                currentDict = currentDict[c]
+            progLetter = progLetter + letter
+            if(progLetter in currentDict):
+                currentDict = currentDict[progLetter]
             else:
                 return 0
         if('value' in currentDict):
@@ -99,24 +97,24 @@ class TreeTrie:
         else:
             return 0
     #It get all values of all words and returns a list with all the values
-    #reomember to compare with the entry list, because it save only the values
+    #remember to compare with the entry list, because it save only the values
     def getAll(self, lista):
         listona = []
         for i in range(len(lista)):
-            if(inTrie(self.tree,lista[i])):
-                listona.append(getVal(self.tree,lista[i]))
+            if(self.inTrie(lista[i])):
+                listona.append(self.getVal(lista[i]))
         return listona
 
-    #you need to pass the Trie Tree and the word you are looking for
+    #As the name says, 'in Trie ?'
     def inTrie(self, word):
         currentDict = self.tree
-        c = '' 
+        progLetter = '' 
         for letter in word:
-            c = c + letter
+            progLetter = progLetter + letter
             #it runs every letter
-            if(c in currentDict):
+            if(progLetter in currentDict):
                 #it will run until the last letter found
-                currentDict = currentDict[c]
+                currentDict = currentDict[progLetter]
             else:
                 return False
         else:
@@ -127,11 +125,16 @@ class TreeTrie:
             else:
                 return False
 
+
+
+#Debugger
 x = TreeTrie('salada')
 print(x.tree)
-x.percorra(x.tree)
-x.addTrie('sal','salvador')
-
+#x.percorra(x.tree) #WRONG
+print(x.addTrie('sal','salvador','salada','salada'))
+ll = ['sal','salvador','salada']
+print(x.getAll(ll))
+print(x.getVal('salada'))
 print(x.inTrie('sergipe'))
 print(x.inTrie('sal'))
 print(x.tree)
@@ -139,8 +142,8 @@ print(x.tree)
 
 #Constructor CHECK
 #AddTrie CHECK
-#percorra NOT CHECK
-#percorraTor NOT CHECK
-#getVal NOT CHECK
-#getAll NOT CHECK
+#percorra NOT CHECK (?)
+#percorraTor NOT CHECK (?)
+#getVal CHECK
+#getAll CHECK
 #inTrie CHECK
